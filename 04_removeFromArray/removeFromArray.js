4e14d504857996f0e7a96f6aaad246df93f6fe57
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-02-26 14:32:06 +0000

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = Array();
+    for (i of arr) {
+        if (args.indexOf(i) == -1) newArr.push(i);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
