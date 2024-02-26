6f83074ce1990514b5f81fe3ad3baf9a5fa0baf3
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-02-26 14:52:18 +0000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstVal, secondVal) {
+    if (typeof firstVal !== "number" || typeof secondVal !== "number") return 'ERROR';
+    if (firstVal < 0 || secondVal < 0) return 'ERROR';
+    let sum = 0;
+    if (firstVal > secondVal){
+        for (let i = secondVal; i <= firstVal; i++){
+            sum += i;
+        }
+    }
+    for (let i = firstVal; i <= secondVal; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
