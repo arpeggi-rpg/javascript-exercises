199cef9732403cf25da01b14f0c59ed1bc882625
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-02-26 13:52:28 +0000

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = string.split('');
+    let reverseArr = Array();
+    for (let i = 0; i < stringArr.length; i++) {
+        reverseArr.splice(0, 0, stringArr.at(i));
+    }
+    return reverseArr.join('');
 };
 
 // Do not edit below this line
